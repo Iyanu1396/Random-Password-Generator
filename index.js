aa808ac3d940fb762047a02fd3ff049f291e4587
Iyanuoluwa <iyanu1396@gmail.com>
2023-01-31 00:16:36 +0100

    code refractored

--- index.js
+++ index.js
@@ -1,40 +1,25 @@
 const characters =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
+const btnEl = document.getElementById("btn-el")
 let passwordOne = document.getElementById("generated1")
 let passwordTwo = document.getElementById("generated2")
 
-
-
-
-
-function randomCharacters1() { 
-
-    let randomIndex =characters[Math.floor(Math.random() * characters.length)] 
-    return randomIndex
-     
+function generate1() {
+    return characters[ Math.floor(Math.random()* characters.length)];
 }
 
-function randomCharacters2() { 
-
-    let randomIndex =characters[Math.floor(Math.random() * characters.length)] 
-    return randomIndex
-     
+function generate2() {
+    return characters[ Math.floor(Math.random()* characters.length)];
 }
 
-function generate() {
-    let randomPassword1 = " "
+btnEl.addEventListener("click" , function() {
+    let renderedPassword1 = " "
+    let renderedPassword2 = " "
     for (let i = 0 ; i < 15 ; i++) {
-     randomPassword1 += randomCharacters1()
+       renderedPassword1 += generate1()
+       renderedPassword2 += generate2()
     }
-   passwordOne.textContent = randomPassword1
-
-   let randomPassword2= " "
-   for (let i = 0 ; i < 15 ; i++) {
-    randomPassword2 += randomCharacters2()
-   }
-  passwordTwo.textContent = randomPassword2
-   
-}
-
-
+    passwordOne.innerHTML = renderedPassword1
+    passwordTwo.innerHTML = renderedPassword2
+});
